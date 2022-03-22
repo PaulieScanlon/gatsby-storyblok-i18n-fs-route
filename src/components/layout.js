059b6131ecdb42ff2fbe67fa588a56511b3063c3
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-22 12:01:19 +0000

    chore: un-rewrite slug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,16 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import Navigation from './navigation'
-import Footer from './Footer'
+import Navigation from "./navigation"
+import Footer from "./Footer"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ children, location }) => {
-  console.log(children)
+  // console.log(children)
   const { settings } = useStaticQuery(graphql`
     query Settings {
-      settings: allStoryblokEntry(filter: {field_component: {eq: "settings"}}) {
+      settings: allStoryblokEntry(
+        filter: { field_component: { eq: "settings" } }
+      ) {
         edges {
           node {
             name
@@ -21,19 +23,23 @@ const Layout = ({ children, location }) => {
   `)
 
   let { pathname } = location
-  let language = pathname.split("/")[1].replace('/', '')
-  let activeLanguage = ['de', 'en'].includes(language) ? language : 'en'
-  let correctSetting = settings.edges.filter(edge => edge.node.full_slug.indexOf(activeLanguage) > -1)
-  let hasSetting = correctSetting && correctSetting.length ? correctSetting[0].node : {}
-  let content = typeof hasSetting.content === 'string' ? JSON.parse(hasSetting.content) : hasSetting.content
+  let language = pathname.split("/")[1].replace("/", "")
+  let activeLanguage = ["de", "en"].includes(language) ? language : "en"
+  let correctSetting = settings.edges.filter(
+    edge => edge.node.full_slug.indexOf(activeLanguage) > -1
+  )
+  let hasSetting =
+    correctSetting && correctSetting.length ? correctSetting[0].node : {}
+  let content =
+    typeof hasSetting.content === "string"
+      ? JSON.parse(hasSetting.content)
+      : hasSetting.content
   let parsedSetting = Object.assign({}, content, { content: content })
 
   return (
     <div className="bg-gray-300">
       <Navigation settings={parsedSetting} lang={activeLanguage} />
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
       <Footer />
     </div>
   )
